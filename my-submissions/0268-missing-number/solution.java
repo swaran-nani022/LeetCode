ec7ec19b36a3e9120b492f1b41f17407c936aadf
Swaran <swaran.nani022@gmail.com>
2025-04-09 13:24:14 +0000

    Sync LeetCode submission - Missing Number - Runtime - 6 ms (18.28%), Memory - 45.1 MB (99.93%)

--- my-submissions/0268-missing-number/solution.java
+++ my-submissions/0268-missing-number/solution.java
@@ -1,12 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
         Arrays.sort(nums);
-        int n=nums.length;
-        int total=n*(n+1)/2;
-        int s=0;
-        for(int i=0;i<nums.length;i++){
-           s+=nums[i];
+        int n=nums.length-1;
+        int l=0;
+        int h=n;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(nums[m]>m) h=m-1;
+            else l=m+1; 
         }
-        return total-s;
+        return l;
     }
 }
