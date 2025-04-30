703d7e6db62f3afd3ed56b0ee4c275cb2f69bbdd
Swaran <swaran.nani022@gmail.com>
2025-04-30 08:28:01 +0000

    Sync LeetCode submission - Merge Two Sorted Lists - Runtime - 0 ms (100.00%), Memory - 42.4 MB (99.99%)

--- my-submissions/0021-merge-two-sorted-lists/solution.java
+++ my-submissions/0021-merge-two-sorted-lists/solution.java
@@ -10,23 +10,21 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy=new ListNode();
-        ListNode current=dummy;
+        
+        ListNode dummy=new ListNode(10);
+        ListNode cur=dummy;
         while(list1 != null && list2 != null){
-            if(list1.val <= list2.val){
-                current.next=list1;
+            if(list1.val < list2.val){
+                cur.next=list1;
                 list1=list1.next;
-            }else{
-                current.next=list2;
+            }
+            else{
+                cur.next=list2;
                 list2=list2.next;
             }
-            current=current.next;
+            cur=cur.next;
         }
-        if(list1!=null){
-            current.next=list1;
-        }else{
-            current.next=list2;
-        }
-    return dummy.next;
+       cur.next=(list1==null)?list2 : list1;
+       return dummy.next;
     }
 }
