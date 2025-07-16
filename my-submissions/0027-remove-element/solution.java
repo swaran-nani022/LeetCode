8a6198f3292d37f2a8e3f435fe79191d961bd977
Swaran <swaran.nani022@gmail.com>
2025-07-16 18:59:35 +0000

    Sync LeetCode submission - Remove Element - Runtime - 0 ms (100.00%), Memory - 41.9 MB (99.98%)

---
+++ my-submissions/0027-remove-element/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n=nums.length;
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=val) nums[c++]=nums[i];
+        }
+        //System.out.println(c);
+        return c;
+    }
+}
