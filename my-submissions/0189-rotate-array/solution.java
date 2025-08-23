0b270e91ac114fea900122e8670b014df3faef7a
Swaran <swaran.nani022@gmail.com>
2025-08-23 11:23:42 +0000

    Sync LeetCode submission - Rotate Array - Runtime - 0 ms (100.00%), Memory - 57.5 MB (99.88%)

---
+++ my-submissions/0189-rotate-array/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+        rev(nums,0,n-1);
+        rev(nums,0,k-1);
+        rev(nums,k,n-1);
+    }
+    public static void rev(int[] arr,int l,int r){
+        while(l<r){
+            int temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            l++;
+            r--;
+        }
+    }
+}
