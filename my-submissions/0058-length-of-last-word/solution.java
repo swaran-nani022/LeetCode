f31fd7f5360a360135884b0df9b5f0fae9ed7a40
Swaran <swaran.nani022@gmail.com>
2025-08-23 12:09:44 +0000

    Sync LeetCode submission - Length of Last Word - Runtime - 0 ms (100.00%), Memory - 42.2 MB (99.87%)

---
+++ my-submissions/0058-length-of-last-word/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int c=0;
+        int i=s.length()-1;
+        while( i>=0 && s.charAt(i)==' ') i--;
+        while( i>=0 && s.charAt(i)!=' '){ c++; i--; }
+        return c;
+    }
+}
