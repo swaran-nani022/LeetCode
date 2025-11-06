10913fb2d8ec109989743a8fec61b29d887c4d2d
Swaran <swaran.nani022@gmail.com>
2025-11-06 13:40:48 +0000

    Sync LeetCode submission - Valid Sudoku - Runtime - 2 ms (69.49%), Memory - 46.4 MB (72.04%)

---
+++ my-submissions/0036-valid-sudoku/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+    HashSet<Character>[] rows=new HashSet[9];
+    HashSet<Character>[] cols=new HashSet[9];
+    HashSet<Character>[] boxes=new HashSet[9];
+    for(int i=0;i<9;i++){
+        rows[i]=new HashSet<>();
+        cols[i]=new HashSet<>();
+        boxes[i]=new HashSet<>();
+    }
+    for(int r=0;r<9;r++){
+        for(int c=0;c<9;c++){
+            char val=board[r][c];
+            if(val=='.')continue;
+            int boxind=(r/3)*3+(c/3);
+            if(rows[r].contains(val)) return false;
+            rows[r].add(val);
+            if(cols[c].contains(val)) return false;
+            cols[c].add(val);
+            if(boxes[boxind].contains(val)) return false;
+            boxes[boxind].add(val);
+        }
+    }
+    return true;
+    }
+}
