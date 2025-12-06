00cb9495f4fa41fef66ae9b7c90423f23a1eb1d4
Swaran <swaran.nani022@gmail.com>
2025-12-06 16:29:23 +0000

    Sync LeetCode submission - Merge Intervals - Runtime - 8 ms (91.64%), Memory - 49.1 MB (60.00%)

---
+++ my-submissions/0056-merge-intervals/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)-> Integer.compare(a[0],b[0]));
+        List<int[]> merged=new ArrayList<>();
+        int[] prev=intervals[0];
+        for(int i=0;i<intervals.length;i++){
+            if(intervals[i][0]<=prev[1]){
+                prev[1]=Math.max(prev[1],intervals[i][1]);
+            }else{
+                merged.add(prev);
+                prev=intervals[i];
+            }
+        }
+        merged.add(prev);
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
