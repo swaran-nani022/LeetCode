5230d1794004f9310512f04910baab375e18616a
Swaran <swaran.nani022@gmail.com>
2025-12-09 15:39:21 +0000

    Sync LeetCode submission - Range Sum Query - Immutable - Runtime - 55 ms (10.18%), Memory - 47 MB (99.62%)

---
+++ my-submissions/0303-range-sum-query---immutable/solution.java
@@ -0,0 +1,20 @@
+class NumArray {
+    int[] pref;
+    public NumArray(int[] nums) {
+        pref=nums;
+    }
+    
+    public int sumRange(int left, int right) {
+        int sum=0;
+        for(int i=left;i<=right;i++){
+            sum+=pref[i];
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
