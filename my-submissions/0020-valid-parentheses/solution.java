c5e8bcc477c544b5cc69623ce30093282c988aec
Swaran <swaran.nani022@gmail.com>
2025-12-11 12:49:02 +0000

    Sync LeetCode submission - Valid Parentheses - Runtime - 2 ms (98.17%), Memory - 43.2 MB (57.74%)

--- my-submissions/0020-valid-parentheses/solution.java
+++ my-submissions/0020-valid-parentheses/solution.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c=='(' || c=='{' || c=='['){
+        for(char c:s.toCharArray()){
+            if(c=='(' || c=='[' || c=='{'){
                 st.push(c);
             }else{
-                if(st.isEmpty()){
-                    return false;
-                }
-            char t=st.pop();
-            if(( c==')'&&t!='(') || (c=='}'&&t!='{') || (c==']'&&t!='[')){
-                return false;
-            } 
+                if(st.isEmpty())return false;
+                char sec=st.pop();
+                if(c==')' && sec!='(') return false;
+                if(c=='}' && sec!='{') return false;
+                if(c==']' && sec!='[') return false;
+            }
         }
+        return st.isEmpty();
     }
-    return st.isEmpty();
-}
 }
