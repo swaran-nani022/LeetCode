bde5d74a10b512522ec27f3b330b2257a63a7c90
Swaran <swaran.nani022@gmail.com>
2026-01-14 19:02:46 +0000

    Sync LeetCode submission - N-Repeated Element in Size 2N Array - Runtime - 0 ms (100.00%), Memory - 47.4 MB (59.36%)

---
+++ my-submissions/1001-n-repeated-element-in-size-2n-array/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int num:nums){
+            if(set.contains(num)){
+                return num;
+            }
+            else{
+                set.add(num);
+            }
+        }
+        return -1;
+    }
+}
