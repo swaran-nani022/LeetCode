9906b211dbfc454093967667270f66517c5a7701
Swaran <swaran.nani022@gmail.com>
2026-03-06 17:51:41 +0000

    Sync LeetCode submission - Sum of Two Integers - Runtime - 0 ms (100.00%), Memory - 42.2 MB (14.68%)

---
+++ my-submissions/0371-sum-of-two-integers/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getSum(int a, int b) {
+        while (a != 0) {
+            int tmp = (a & b) << 1;
+            b = a ^ b;
+            a = tmp;
+        }
+        return b;
+    }
+}
