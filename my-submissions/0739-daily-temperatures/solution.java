09389b0221ea02245bc5cb3d28473160a4d88949
Swaran <swaran.nani022@gmail.com>
2026-03-12 19:45:12 +0000

    Sync LeetCode submission - Daily Temperatures - Runtime - 60 ms (80.05%), Memory - 107.7 MB (32.43%)

---
+++ my-submissions/0739-daily-temperatures/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+    int n=temperatures.length;
+    int[] arr=new int[n];
+    Stack<Integer> st=new Stack<>();
+    for(int i=0;i<n;i++){
+        while(!st.isEmpty() && temperatures[i]>temperatures[st.peek()]){
+            int pre=st.pop();
+            arr[pre]=i-pre;
+        }
+        st.push(i);
+    }
+    return arr;
+    }
+}
