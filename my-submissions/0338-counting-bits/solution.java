dad1c7e3b2b6e4fa0de9b16cbec1f89841af7273
Swaran <swaran.nani022@gmail.com>
2026-03-26 16:18:49 +0000

    Sync LeetCode submission - Counting Bits - Runtime - 2 ms (96.45%), Memory - 48.9 MB (5.08%)

--- my-submissions/0338-counting-bits/solution.java
+++ my-submissions/0338-counting-bits/solution.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] arr=new int[n+1];
-        for(int i=0;i<=n;i++){
-            int c=0;
-            int ii=i;
-            while(ii!=0){
-                ii=ii&(ii-1);
-                c++;
+        int[] dp=new int[n+1];
+        int off=1;
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            if(off*2==i){
+                off=i;
             }
-            arr[i]=c;
+            dp[i]=1+dp[i-off];
         }
-        return arr;
+        return dp;
     }
 }
