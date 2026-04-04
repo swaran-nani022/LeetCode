e0f6a2ed0dd4edf644ad7fd45670dea071cbd0b4
Swaran <swaran.nani022@gmail.com>
2026-04-04 15:01:57 +0000

    Sync LeetCode submission - Climbing Stairs - Runtime - 0 ms (100.00%), Memory - 42.1 MB (54.26%)

--- my-submissions/0070-climbing-stairs/solution.java
+++ my-submissions/0070-climbing-stairs/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return steps(n,dp);
-    }
-    int steps(int n,int dp[]){
-        if(n==0) return 1;
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int a=steps(n-1,dp);
-        int b=steps(n-2,dp);
-        return dp[n]=a+b;
+        int p=2;
+        int p2=1;
+        int c=0;
+        if(n==1)return p2;
+        if(n==2)return p;
+        for(int i=3;i<=n;i++){
+            c=p+p2;
+            p2=p;
+            p=c;
+        }
+        return p;
     }
 }
