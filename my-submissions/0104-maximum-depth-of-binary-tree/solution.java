7de80ba214ba1c95e33377c6bdd245f3ace7512e
Swaran <swaran.nani022@gmail.com>
2026-05-31 17:11:06 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree - Runtime - 0 ms (100.00%), Memory - 44.4 MB (67.91%)

---
+++ my-submissions/0104-maximum-depth-of-binary-tree/solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null)return 0;
+        int mr=maxDepth(root.right);
+        int ml=maxDepth(root.left);
+        return Math.max(mr,ml)+1;
+    }
+}
