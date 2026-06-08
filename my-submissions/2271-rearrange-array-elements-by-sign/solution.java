92d39599e30716a992c32acbee5953477691c43e
Swaran <swaran.nani022@gmail.com>
2026-06-08 05:42:32 +0000

    Sync LeetCode submission - Rearrange Array Elements by Sign - Runtime - 3 ms (100.00%), Memory - 175.6 MB (28.16%)

---
+++ my-submissions/2271-rearrange-array-elements-by-sign/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+       int pos=0,neg=1,n=nums.length;
+       int[] arr=new int[n];
+       for(int i=0;i<n;i++){
+        if(nums[i]>0){ arr[pos]=nums[i]; pos+=2; }
+        else{ arr[neg]=nums[i]; neg+=2; }
+       }
+       return arr;
+    }
+}
