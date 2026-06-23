49b6df38ec112321c1882d1b51849edec4ca1929
Swaran <swaran.nani022@gmail.com>
2026-06-23 08:37:36 +0000

    Sync LeetCode submission - Final Value of Variable After Performing Operations - Runtime - 0 ms (100.00%), Memory - 44.7 MB (13.19%)

---
+++ my-submissions/2137-final-value-of-variable-after-performing-operations/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(String str:operations){
+            if(str.charAt(1)=='-'){x--;}
+            else {x++;}
+        }
+        return x;
+    }
+}
