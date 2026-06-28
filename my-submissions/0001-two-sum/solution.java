fc11570e2afebd7e3f26b82b26fc3c32ce73c70c
Swaran <swaran.nani022@gmail.com>
2026-06-28 06:53:42 +0000

    Sync LeetCode submission - Two Sum - Runtime - 2 ms (99.33%), Memory - 47.4 MB (10.98%)

--- my-submissions/0001-two-sum/solution.java
+++ my-submissions/0001-two-sum/solution.java
@@ -1,22 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        for(int i = 1; i < nums.length; i++){
-
-            // here i is expanding the range (jump length)
-
-            for(int j = 0; j < nums.length-i; j++){
-
-                // here i just minimizing the iterations
-
-                if((nums[j] + nums[j+i]) == target){
-                    return new int[]{j, j+i}; 
-                }
-
+        Map<Integer,Integer> hm=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int want=target-nums[i];
+            if(hm.containsKey(want)){
+                return new int[]{hm.get(want),i};
             }
-
+            hm.put(nums[i],i);
         }
-
-        return null;
+        return new int[]{};
     }
 }
