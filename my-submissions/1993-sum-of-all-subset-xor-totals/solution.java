a2e7057fc73c956a6ce842cb77ee32a47af7de25
Swaran <swaran.nani022@gmail.com>
2026-06-29 17:57:35 +0000

    Sync LeetCode submission - Sum of All Subset XOR Totals - Runtime - 5 ms (26.34%), Memory - 42.8 MB (44.10%)

---
+++ my-submissions/1993-sum-of-all-subset-xor-totals/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int totalSum = 0;
+        int n = nums.length;
+        int totalSubsets = 1 << n;
+        for (int i = 0; i < totalSubsets; i++) {
+            int currentXOR = 0;
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    currentXOR ^= nums[j];
+                }
+            }
+            totalSum += currentXOR;
+        }
+        
+        return totalSum;
+    }
+}
