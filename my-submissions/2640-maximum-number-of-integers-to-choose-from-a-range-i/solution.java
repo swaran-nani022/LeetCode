207fb32862b8340abeddf9a98eb73947359474be
Swaran <swaran.nani022@gmail.com>
2026-07-07 19:16:38 +0000

    Sync LeetCode submission - Maximum Number of Integers to Choose From a Range I - Runtime - 5 ms (92.68%), Memory - 48.2 MB (57.84%)

---
+++ my-submissions/2640-maximum-number-of-integers-to-choose-from-a-range-i/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int maxCount(int[] banned, int n, int maxSum) {
+        int s = banned.length;
+        
+        int[] arr = new int[10001];
+        for(int i = 0; i < s; i++) {
+            arr[banned[i]] = 1;
+        }
+
+        long sum = 0;
+        int cnt = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(arr[i] == 1)
+                continue;
+            sum += i;
+            if(sum > maxSum)
+                break;
+            cnt++;
+        }
+        return cnt;
+    }
+}
