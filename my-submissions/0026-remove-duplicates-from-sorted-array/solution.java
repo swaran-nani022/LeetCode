1f90a56c0c9acf358bf5507030500fa7314495dc
Swaran <swaran.nani022@gmail.com>
2026-07-09 19:20:28 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array - Runtime - 0 ms (100.00%), Memory - 47.1 MB (9.19%)

--- my-submissions/0026-remove-duplicates-from-sorted-array/solution.java
+++ my-submissions/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,13 +1,14 @@
 class Solution {
-    public int removeDuplicates(int[] arr) {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0)return 0;
         int i=0;
-        for(int j=1;j<arr.length;j++){
-            if(arr[i]!=arr[j]){
+        for(int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[i]){
                 i++;
-                arr[i]=arr[j];
+                nums[i]=nums[j];
             }
         }
         return i+1;
-        
     }
 }
+
