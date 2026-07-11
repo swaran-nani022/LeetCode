92c0ecbe98259091494f694f191ead934c87d6c4
Swaran <swaran.nani022@gmail.com>
2026-07-11 06:20:43 +0000

    Sync LeetCode submission - Find the Index of the First Occurrence in a String - Runtime - 1 ms (47.97%), Memory - 42.9 MB (68.12%)

--- my-submissions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
+++ my-submissions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
@@ -1,8 +1,39 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
-       for(int i=0,j=needle.length();j<=haystack.length();i++,j++){
-        if(haystack.substring(i,j).equals(needle)) return i;
-       } 
-       return -1;
+    public int strStr(String text, String pat) {
+        int m=text.length();
+        int n=pat.length();
+        if(m<n){return -1;}
+        int base=256;
+        int mod=10007;
+        int h=1;
+        for(int i=0;i<n-1;i++){
+            h=(h*base)%mod;
+            }
+        int wh=0;
+        int ph=0;
+        for(int i=0;i<n;i++){
+            wh=(base*wh+text.charAt(i))%mod;
+            ph=(base*ph+pat.charAt(i))%mod;
+        }
+        for(int i=0;i<=m-n;i++){
+            if(wh==ph){
+            int j=0;
+            while(j<n){
+                if(text.charAt(i+j)!=pat.charAt(j)){
+                    break;
+                }
+                j++;
+            }
+            if(j==n){return i;}
+            }
+            if(i<m-n){
+            wh=(wh-text.charAt(i)*h)%mod;
+            wh=(wh*base+text.charAt(i+n))%mod;
+            if(wh<0){
+                wh+=mod;
+            }
+            }
+        }
+        return -1;
     }
 }
