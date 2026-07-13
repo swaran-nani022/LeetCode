ae53042f3dbdf136fc20b04de8e9fa67c7538041
Swaran <swaran.nani022@gmail.com>
2026-07-13 17:47:03 +0000

    Sync LeetCode submission - Sequential Digits - Runtime - 0 ms (100.00%), Memory - 42.6 MB (22.09%)

---
+++ my-submissions/1212-sequential-digits/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> ar=new ArrayList<>();
+        for(int i=1;i<=9;i++){
+            int num=i;
+            for(int j=i+1;j<=9;j++){
+                num=num*10+j;
+                if(num>=low && num<=high)ar.add(num);
+            }
+        }
+        Collections.sort(ar);
+        return ar;
+    }
+}
