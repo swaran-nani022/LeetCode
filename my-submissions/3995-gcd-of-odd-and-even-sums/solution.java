a537d8e3f11a29212f933b13a53e24b6b5cc8954
Swaran <swaran.nani022@gmail.com>
2026-07-15 19:31:20 +0000

    Sync LeetCode submission - GCD of Odd and Even Sums - Runtime - 1 ms (83.02%), Memory - 42.8 MB (6.14%)

--- my-submissions/3995-gcd-of-odd-and-even-sums/solution.java
+++ my-submissions/3995-gcd-of-odd-and-even-sums/solution.java
@@ -1,5 +1,10 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-    return n;
-}
+        int a=gcd(n*n,n*(n+1));
+        return a;
+    }
+    public int gcd(int a,int b){
+        if(b==0)return a;
+        return gcd(b,a%b);
+    }
 }
