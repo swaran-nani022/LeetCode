3a8b304e77d8bfc31ab347e4419987c557c4b8c9
Swaran <swaran.nani022@gmail.com>
2026-07-19 16:45:29 +0000

    Sync LeetCode submission - Rearrange String to Avoid Character Pair - Runtime - 4 ms (0.00%), Memory - 45.6 MB (0.00%)

---
+++ my-submissions/4355-rearrange-string-to-avoid-character-pair/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String rearrangeString(String s, char x, char y) {
+        StringBuilder sb=new StringBuilder();
+        StringBuilder sb1=new StringBuilder();
+        StringBuilder sb2=new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(c==y)sb.append(c);
+            else if(c==x)sb1.append(c);
+            else sb2.append(c);
+        }
+        return sb.toString()+sb2.toString()+sb1.toString();
+    }
+}
